9f9eae8074f6fe891839802f74ffbb4f0e34c4e5
Eddy <eddy.wanny@crypto.com>
2020-12-03 19:26:53 +0300

    Add tests for importing a main-net wallet

--- src/service/WalletImporter.spec.ts
+++ src/service/WalletImporter.spec.ts
@@ -19,4 +19,20 @@ describe('Testing WalletImporter', () => {
     expect(testNetWallet.address).to.eq('tcro15rsn69ze9r7g52tk0u6cyhu4edep88dxgtzm65');
     expect(testNetWallet.config).to.eq(testNetConfig);
   });
+
+  it('Test importing wallet with main-net configuration', () => {
+    const mainNetConfig = DefaultWalletConfigs.MainNetConfig;
+
+    const importOptions: WalletImportOptions = {
+      config: mainNetConfig,
+      phrase:
+        'team school reopen cave banner pass autumn march immune album hockey region baby critic insect armor pigeon owner number velvet romance flight blame tone',
+      walletName: 'My-MainNet-Wallet',
+    };
+    const testNetWallet = WalletImporter.import(importOptions);
+
+    expect(testNetWallet.name).to.eq('My-MainNet-Wallet');
+    expect(testNetWallet.address).to.eq('cro1n0ejfh2ur2nslekrynvcwuwc9cccnhxfqn6sfs');
+    expect(testNetWallet.config).to.eq(mainNetConfig);
+  });
 });
