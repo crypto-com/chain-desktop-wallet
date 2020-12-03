ed075dc8bcd5171d749aa80fc4bcac40e74d4ee0
crypto-matto <matthew.to@crypto.com>
2020-12-03 13:02:33 +0800

    Refined layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './components/App/App';
 import WelcomePage from './pages/welcome';
 import * as serviceWorker from './serviceWorker';
 
--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,28 +1,30 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+// import ReactDOM from 'react-dom';
 import './index.scss';
-import logo from '../../assets/logo-cdc.png';
 import 'antd/dist/antd.css';
 import { Button } from 'antd';
+import logo from '../../assets/logo-products-chain.svg';
+
 function WelcomePage() {
-    return (
-        <div className="welcome-page">
-            <div className="header">
-                <img src={logo} className="logo" alt="logo" />
-            </div>
-            <div className="container">
-                <div>
-                    <div className="title">Crypto.com Chain Wallet</div>
-                    <div className="slogan">Our Sample Chain Wallet supports wallet management and funds transfer.</div>
-                    <div className="button-container">
-                        <Button>Restore Wallet</Button>
-                        <Button type="primary">Create Wallet</Button>
-                    </div>  
-                </div>
-                
-            </div>
+  return (
+    <main className="welcome-page">
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <div className="container">
+        <div>
+          <div className="title">Crypto.com Chain Wallet</div>
+          <div className="slogan">
+            Our Sample Chain Wallet supports wallet management and funds transfer.
+          </div>
+          <div className="button-container">
+            <Button>Restore Wallet</Button>
+            <Button type="primary">Create Wallet</Button>
+          </div>
         </div>
-    )
+      </div>
+    </main>
+  );
 }
 
-export default WelcomePage
+export default WelcomePage;
