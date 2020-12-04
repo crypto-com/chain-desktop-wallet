34fac7914e3156f6fbaf7e08b984a68f46cd83c0
crypto-matto <matthew.to@crypto.com>
2020-12-04 17:27:35 +0800

    Redirect to welcome page if path not found

--- src/pages/route.tsx
+++ src/pages/route.tsx
@@ -1,66 +1,60 @@
 import React from 'react';
 // import React, { useState } from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import WelcomePage from './welcome';
 import RestorePage from './restore';
 import CreatePage from './create';
 
 function RouteHub() {
+  // const [page, setPage] = useState('welcome');
+  const routeIndex = {
+    name: 'Welcome Page',
+    key: 'welcome',
+    path: '/',
+    component: <WelcomePage />,
+  };
 
-    // const [page, setPage] = useState('welcome');
-    const routeIndex = {
-        name: 'Welcome Page',
-        key: 'welcome',
-        path: '/',
-        component: <WelcomePage />
-    }
+  const routeItems = [
+    {
+      name: 'Welcome Page',
+      key: 'welcome',
+      path: '/welcome',
+      component: <WelcomePage />,
+    },
+    {
+      name: 'Restore Page',
+      key: 'restore',
+      path: '/restore',
+      component: <RestorePage />,
+    },
+    {
+      name: 'Create Page',
+      key: 'create',
+      path: '/create',
+      component: <CreatePage />,
+    },
+  ];
 
-    const routeItems = [
-        {
-            name: 'Welcome Page',
-            key: 'welcome',
-            path: '/welcome',
-            component: <WelcomePage />
-        },
-        {
-            name: 'Restore Page',
-            key: 'restore',
-            path: '/restore',
-            component: <RestorePage />
-        },
-        {
-            name: 'Create Page',
-            key: 'create',
-            path: '/create',
-            component: <CreatePage />
-        },
-    ]
-
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route exact path={routeIndex.path} key={routeIndex.key}>
-                        {routeIndex.component}
-                    </Route>
-                    {
-                        routeItems.map((item) => {
-                            return (
-                                <Route path={item.path} key={item.path}>
-                                    {item.component}
-                                </Route>
-                            )
-                        })
-                    }
-                </Switch>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path={routeIndex.path} key={routeIndex.key}>
+            {routeIndex.component}
+          </Route>
+          {routeItems.map(item => {
+            return (
+              <Route path={item.path} key={item.path}>
+                {item.component}
+              </Route>
+            );
+          })}
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default RouteHub
+export default RouteHub;
