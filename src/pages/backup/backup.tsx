70ac2331d9bcd007f8a044fc70e0436545b2794a
crypto-matto <matthew.to@crypto.com>
2020-12-09 11:57:45 +0800

    Added copy button

--- src/pages/backup/backup.tsx
+++ src/pages/backup/backup.tsx
@@ -3,6 +3,8 @@ import { useHistory } from 'react-router-dom';
 import { atom, useRecoilValue } from 'recoil';
 import './backup.less';
 import { Button, Checkbox } from 'antd';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import MouseTooltip from 'react-sticky-mouse-tooltip';
 import logo from '../../assets/logo-products-chain.svg';
 
 const encryptedPhraseState = atom({
@@ -13,6 +15,7 @@ const encryptedPhraseState = atom({
 function BackupPage() {
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
   const [encryptedPhrase, setEncryptedPhrase] = useState('none');
+  const [mouseTooltip, setMouseTooltip] = useState(false);
   const phrase: string = useRecoilValue(encryptedPhraseState);
   const didMountRef = useRef(false);
   const history = useHistory();
@@ -36,6 +39,13 @@ function BackupPage() {
     }
   }, [encryptedPhrase, history, phrase]);
 
+  const onCopyClick = () => {
+    setMouseTooltip(true);
+    setTimeout(() => {
+      setMouseTooltip(false);
+    }, 100);
+  };
+
   return (
     <main className="backup-page">
       <div className="header">
@@ -50,16 +60,28 @@ function BackupPage() {
             You would need your recovery phrase to restore and access wallet.
           </div>
           <div>
-            <div className="phrase-container">
-              {phrase.split(' ').map((item, index) => {
-                return (
-                  <div className="phrase" key={index}>
-                    <span>{index + 1}. </span>
-                    {item}
-                  </div>
-                );
-              })}
-            </div>
+            <CopyToClipboard text={phrase}>
+              <div onClick={onCopyClick}>
+                <div className="phrase-container">
+                  {phrase.split(' ').map((item, index) => {
+                    return (
+                      <div className="phrase" key={index}>
+                        <span>{index + 1}. </span>
+                        {item}
+                      </div>
+                    );
+                  })}
+                </div>
+                <MouseTooltip
+                  offsetX={15}
+                  offsetY={0}
+                  className={`mouse-tooltip ${mouseTooltip ? '' : 'hide'}`}
+                >
+                  <span>Copied!</span>
+                </MouseTooltip>
+              </div>
+            </CopyToClipboard>
+
             <div>
               <Checkbox onChange={checkboxOnChange}>
                 I understand the recovery phrase will be only shown once
