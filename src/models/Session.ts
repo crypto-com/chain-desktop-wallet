5c0fd90b94745804fdc38b49b6358328f9b5c55c
Eddy <eddy.wanny@crypto.com>
2020-12-10 08:28:56 +0300

    initial setup

--- src/models/Session.ts
+++ src/models/Session.ts
@@ -4,11 +4,14 @@ export class Session {
   // Session holds currently selected wallet
   public readonly wallet: Wallet;
 
+  public readonly currency: string;
+
   // TODO : More sessions data to be added in subsequent tasks
 
   public static SESSION_ID = 'SESSION_ID';
 
-  constructor(wallet: Wallet) {
+  constructor(wallet: Wallet, currency: string = 'USD') {
     this.wallet = wallet;
+    this.currency = currency;
   }
 }
