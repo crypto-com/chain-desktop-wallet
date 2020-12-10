5a2a564e2a696880aabaf3c7b3f20ca3045143c5
crypto-matto <matthew.to@crypto.com>
2020-12-10 23:12:25 +0800

    Added Tabs

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import './home.less';
 import 'antd/dist/antd.css';
-import { Layout, Menu, Table, Tag, Space } from 'antd';
-import MenuContentBlock from '../../components/MenuContentBlock/MenuContentBlock';
+import { Layout, Menu, Table, Space, Dropdown, Tabs } from 'antd';
+// import MenuContentBlock from '../../components/MenuContentBlock/MenuContentBlock';
 // import { Link } from 'react-router-dom';
 
 const { Header, Content, Footer, Sider } = Layout;
 const siderWidth = '256px';
-const columns = [
+
+const TransactionColumns = [
   {
     title: 'Index',
     dataIndex: 'index',
@@ -26,60 +27,110 @@ const columns = [
     key: 'amount',
   },
   {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: tags => (
-      <>
-        {tags.map(tag => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
+    title: 'Time',
+    dataIndex: 'time',
+    key: 'time',
   },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (text: any, record: any) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
+
 ];
 
-const data = [
+const TransactionData = [
   {
     key: '1',
     index: '1',
     txhash: '0x0edbd6…e0470dc1',
     amount: '500, 000',
-    tags: ['nice', 'developer'],
+    time: '31/12/2020 23:59:59',
   },
   {
     key: '2',
     index: '2',
     txhash: '0x7a53b75…8104be15',
     amount: '500, 000',
-    tags: ['loser'],
+    time: '31/12/2020 23:59:59',
   },
   {
     key: '3',
     index: '3',
     txhash: '0x7a53b75…8104be15',
     amount: '500, 000',
+    time: '31/12/2020 23:59:59',
+  },
+];
+
+const StakingColumns = [
+  {
+    title: 'Index',
+    dataIndex: 'index',
+    key: 'index',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+    key: 'amount',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: () => (
+      <Space size="middle">
+        <a>Deposit Stake</a>
+        <a>Unbond Stake</a>
+      </Space>
+    ),
+  },
+];
+
+const StakingData = [
+  {
+    key: '1',
+    index: '1',
+    address: 'tcro1reyshfdygf7673xm9p8v0xvtd96m6cd6dzswyj',
+    amount: '500, 000',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    index: '2',
+    address: 'tcro1reyshfdygf7673xm9p8v0xvtd96m6cd6dzswyj',
+    amount: '500, 000',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    index: '3',
+    address: 'tcro1reyshfdygf7673xm9p8v0xvtd96m6cd6dzswyj',
+    amount: '500, 000',
     tags: ['cool', 'teacher'],
   },
 ];
+
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+        Create Wallet
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+        Restore Wallet
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+        Wallet List
+      </a>
+    </Menu.Item>
+  </Menu>
+);
+const { TabPane } = Tabs;
 
 function HomePage() {
   return (
@@ -95,18 +146,18 @@ function HomePage() {
           width={siderWidth}
         >
           <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
             <Menu.Item key="1">
-                Home
+              Home
             </Menu.Item>
             <Menu.Item key="2">
-                Address
+              Address
             </Menu.Item>
             <Menu.Item key="3">
-                Send
+              Send
             </Menu.Item>
             <Menu.Item key="4">
-                Receive
+              Receive
             </Menu.Item>
           </Menu>
           {/* <div className='back-to-welcome'>
@@ -114,10 +165,16 @@ function HomePage() {
               Back to welcome
             </Link>
           </div> */}
+
+          <Dropdown overlay={menu} placement="topCenter">
+            <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+              Wallet - Test 1{/* Hover me <DownOutlined /> */}
+            </a>
+          </Dropdown>
         </Sider>
         <Layout className="site-layout" style={{ marginLeft: siderWidth }}>
           <Header className="site-layout-background" style={{ padding: 0, background: '#f0f2f5' }} >Welcome Back!</Header>
-          <Content style={{ margin: '24px 16px 0', overflow: 'initial', height: 'calc(100vh - 176px)' }}>
+          <Content style={{ margin: '24px 16px 0' }}>
             <div className="site-layout-background balance-container" style={{ padding: 24, textAlign: 'center' }}>
               <div className="balance">
                 <div className="title">TOTAL BALANCE</div>
@@ -128,9 +185,17 @@ function HomePage() {
                 <div className="quantity">250,000 CRO</div>
               </div>
             </div>
-            <MenuContentBlock title='Transaction' className="table">
-              <Table columns={columns} dataSource={data} />
-            </MenuContentBlock>
+            <Tabs defaultActiveKey="1">
+              <TabPane tab="Transactions" key="1">
+                <Table columns={TransactionColumns} dataSource={TransactionData} style={{ minHeight: 500 }} />
+              </TabPane>
+              <TabPane tab="Staking" key="2">
+                <Table columns={StakingColumns} dataSource={StakingData} style={{ minHeight: 500 }} />
+              </TabPane>
+            </Tabs>
+            {/* <MenuContentBlock title='Transaction' className="table">
+              <Table columns={columns} dataSource={data} style={{ minHeight: 500 }} />
+            </MenuContentBlock> */}
           </Content>
           <Footer style={{ textAlign: 'center' }} />
         </Layout>
