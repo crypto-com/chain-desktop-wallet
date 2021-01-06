b05743c76038f3208ae8c2cc7715f20cbda3d375
crypto-matto <matthew.to@crypto.com>
2021-01-06 16:51:56 +0800

    Added fields

--- src/pages/settings/settings.tsx
+++ src/pages/settings/settings.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './settings.less';
 import 'antd/dist/antd.css';
-import { Button, Form, Input, Layout, Select, Tabs } from 'antd';
+import { Button, Form, Input, Layout, Tabs } from 'antd';
 // import {ReactComponent as HomeIcon} from '../../assets/icon-home-white.svg';
 
 const { Header, Content, Footer } = Layout;
 const { TabPane } = Tabs;
-const { Option } = Select;
 const layout = {
   // labelCol: { span: 8 },
   // wrapperCol: { span: 16 },
@@ -15,132 +14,118 @@ const tailLayout = {
   // wrapperCol: { offset: 8, span: 16 },
 };
 
-// const FormGeneral = () => {
-//   return (
-//     <Form.Item name="name" label="Name">
-//       <Input />
-//     </Form.Item>
-//   )
-// }
+const FormGeneral = () => {
+  return (
+    <>
+      <Form.Item
+        name="name"
+        label="Name"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="derivationPath"
+        label="Derivation Path"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="nodeUrl"
+        label="Node Url"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+    </>
+  );
+};
 
-// const FormNetwork = () => {
-//   return (
-//     <Form.Item name="address" label="Address Prefix">
-//     <Input />
-//   </Form.Item>
-//   )
-// }
-
-// const FormSettings = () => {
-//   const [form] = Form.useForm();
-//   const didMountRef = useRef(false);
-
-//   useEffect(() => {
-//     if (!didMountRef.current) {
-//       didMountRef.current = true;
-//     } else {
-//       // Jump to backup screen after walletIdentifier created & setWalletIdentifier finished
-//       console.log('hi')
-//     }
-//   }, []);
-
-//   const onFinish = (values: any) => {
-//     console.log('Success:', values);
-//     console.log('Success:', form.getFieldsValue());
-//   };
-
-//   const onFinishFailed = (errorInfo: any) => {
-//     console.log('Failed:', errorInfo);
-//   };
-
-//   const onReset = () => {
-//     form.resetFields();
-//   };
-
-//   const onFill = () => {
-//     form.setFieldsValue({
-//       name: 'Hello world!',
-//       address: 'testnet id',
-//     });
-//   };
-//   return (
-//     <Form
-//       {...layout}
-//       name="basic"
-//       initialValues={{ remember: true }}
-//       onFinish={onFinish}
-//       onFinishFailed={onFinishFailed}
-//     >
-//       {/* <Tabs defaultActiveKey="1">
-//         <TabPane tab="General" key="1">
-//           <div className="site-layout-background stake-content">
-//             <div className="container">
-//               <div className="description">Withdraw rewards from delegated funds.</div>
-//               <FormGeneral />
-//             </div>
-//           </div>
-//         </TabPane>
-//         <TabPane tab="Network" key="2">
-//           <div className="site-layout-background stake-content">
-//             <div className="container">
-//               <div className="description">Delegate funds to validator.</div>
-//               <FormNetwork />
-//             </div>
-//           </div>
-//         </TabPane>
-//       </Tabs> */}
-//       <Form.Item name="coin" label="Coin">
-//     <Input />
-//   </Form.Item>
-//       <Form.Item name="symbol" label="Symbol">
-//     <Input />
-//   </Form.Item>
-//       <Form.Item {...tailLayout}>
-//         <Button type="primary" htmlType="submit">
-//           Submit
-//         </Button>
-//         <Button htmlType="button" onClick={onReset}>
-//           Reset
-//         </Button>
-//         <Button type="link" htmlType="button" onClick={onFill}>
-//           Fill form
-//         </Button>
-//       </Form.Item>
-//     </Form>
-//   )
-// }
-
+const FormNetwork = () => {
+  return (
+    <>
+      <Form.Item
+        name="address"
+        label="Address Prefix"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="chainId"
+        label="Chain ID"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+    </>
+  );
+};
+const FormCoin = () => {
+  return (
+    <>
+      <Form.Item
+        name="baseDenom"
+        label="Base Denom"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="croDenom"
+        label="CRO Denom"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+    </>
+  );
+};
 const FormSettings = () => {
   const [form] = Form.useForm();
-
-  const onGenderChange = (value: string) => {
-    switch (value) {
-      case 'male':
-        form.setFieldsValue({ note: 'Hi, man!' });
-        break;
-      case 'female':
-        form.setFieldsValue({ note: 'Hi, lady!' });
-        break;
-      case 'other':
-        form.setFieldsValue({ note: 'Hi there!' });
-        break;
-      default:
-        form.setFieldsValue({ note: 'Hi' });
-    }
-  };
 
   const onFinish = values => {
     console.log(values);
   };
 
-  const onReset = () => {
-    form.resetFields();
-  };
-
   const onFill = () => {
     form.setFieldsValue({
       name: 'Hello world!',
-      gender: 'male',
+      nodeUrl: 'https://testnet-croeseid.crypto.com',
+      derivationPath: "m/44'/1'/0/0/0",
+      address: 'tcro',
+      chainId: 'testnet-croeseid-2',
+      baseDenom: 'TCRO',
+      croDenom: 'TCRO',
     });
   };
 
@@ -150,45 +135,24 @@ const FormSettings = () => {
         <TabPane tab="General" key="1">
           <div className="site-layout-background stake-content">
             <div className="container">
-              <div className="description">Withdraw rewards from delegated funds.</div>
-              <Form.Item
-                name="name"
-                label="Name"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
+              <div className="description">General settings</div>
+              <FormGeneral />
             </div>
           </div>
         </TabPane>
         <TabPane tab="Network" key="2">
           <div className="site-layout-background stake-content">
             <div className="container">
-              <div className="description">Delegate funds to validator.</div>
-              <Form.Item
-                name="gender"
-                label="Gender"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
-              >
-                <Select
-                  placeholder="Select a option and change input text above"
-                  onChange={onGenderChange}
-                  allowClear
-                >
-                  <Option value="male">male</Option>
-                  <Option value="female">female</Option>
-                  <Option value="other">other</Option>
-                </Select>
-              </Form.Item>
-              
+              <div className="description">Network settings</div>
+              <FormNetwork />
+            </div>
+          </div>
+        </TabPane>
+        <TabPane tab="Coin" key="3">
+          <div className="site-layout-background stake-content">
+            <div className="container">
+              <div className="description">Coin settings</div>
+              <FormCoin />
             </div>
           </div>
         </TabPane>
@@ -216,13 +180,10 @@ const FormSettings = () => {
       </Form.Item>
       <Form.Item {...tailLayout}>
         <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-        <Button htmlType="button" onClick={onReset}>
-          Reset
+          Save
         </Button>
         <Button type="link" htmlType="button" onClick={onFill}>
-          Fill form
+          Default Settings
         </Button>
       </Form.Item>
     </Form>
