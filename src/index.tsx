ab597543e07d3dba40df53eb6e552c10fb8140dc
jonghwan <jonghwan@crypto.com>
2021-01-07 13:02:53 +0900

    temp code for test

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,29 @@ import * as serviceWorker from './serviceWorker';
 import RouteHub from './pages/route';
 import './index.less';
 import { task } from './service/tasks/BackgroundJob';
+import { IpcRender } from './service/signers/IpcRender';
 
+const ipcRender = new IpcRender();
+function enableWallet() {
+  ipcRender.userEnableWallet();
+}
+function signMessage() {
+  ipcRender.userSignMessage();
+}
 ReactDOM.render(
   <RecoilRoot>
+    <button type="button" onClick={enableWallet}>
+      {' '}
+      Enable Wallet{' '}
+    </button>
+    <button type="button" onClick={signMessage}>
+      {' '}
+      Sign Message{' '}
+    </button>
     <RouteHub />
   </RecoilRoot>,
   document.getElementById('root'),
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/service/signers/IpcRender.ts
+++ src/service/signers/IpcRender.ts
@@ -26,4 +26,40 @@ export class IpcRender implements ISignerProvider {
     const arg = electron.ipcRenderer.sendSync('signMessage', stringMessage);
     return Bytes.fromBuffer(Buffer.from(arg.signed));
   }
+
+  userEnableWallet() {
+    const arg = electron.ipcRenderer.sendSync('enableWallet', {
+      index: 0,
+      message: 'this is the enable wallet request',
+    });
+    const signerAddress = arg.account;
+    const signerPubKey = Bytes.fromBuffer(Buffer.from(arg.pubKey));
+    console.log(+new Date(), '    ', JSON.stringify(arg)); // "pong"이 출력됩니다
+    console.log(signerAddress);
+    console.log(signerPubKey);
+  }
+  async userSignMessage() {
+    const signerAddress = await this.getAddress(0);
+    const fee = { amount: [{ amount: '5000', denom: 'cro' }], gas: '2000000' };
+    const value = {
+      amount: { amount: '1000000', denom: 'cro' },
+      from_address: signerAddress,
+      to_address: signerAddress,
+    };
+    const msg = { type: 'cosmos-sdk/MsgSend', value };
+    const rawMessage = {
+      account_number: '1',
+      chain_id: 'earth',
+      fee,
+      memo: 'mymemo',
+      msgs: [msg],
+      sequence: '0',
+    };
+    const stringMessage = JSON.stringify(rawMessage);
+    const bufferMessage = Buffer.from(stringMessage);
+    const bytesMessage = Bytes.fromBuffer(bufferMessage);
+    const reply = await this.sign(bytesMessage); // json string
+    const hex = reply.toHexString();
+    console.log(new Date(), '    ', hex);
+  }
 }
