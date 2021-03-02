c66c9d41cfaac2558f83b608c622fd14ef270a70
jonghwan <jonghwan@crypto.com>
2021-03-02 19:51:28 +0900

    fix unplug & plug

--- electron/LedgerSignerNative.ts
+++ electron/LedgerSignerNative.ts
@@ -1,15 +1,27 @@
 import { LedgerSigner } from '../src/service/signers/LedgerSigner';
-const TransportHID: any = require('@ledgerhq/hw-transport-node-hid').default;
+import TransportHID from '@ledgerhq/hw-transport-node-hid';
 const CosmosApp: any = require('ledger-cosmos-js').default;
 export class LedgerSignerNative extends LedgerSigner {
+  public transport: TransportHID | null;
+
   constructor(account: number = 0) {
     super(account);
+    this.transport = null;
   }
 
   async createTransport() {
     if (this.app === null || this.app === undefined) {
-      const transport = await TransportHID.open();
+      const transport = await TransportHID.open(''); // take first device
       this.app = new CosmosApp(transport);
+      this.transport = transport;
+    }
+  }
+
+  async closeTransport() {
+    if (this.transport != null) {
+      this.transport.close();
+      this.transport = null;
+      this.app = null;
     }
   }
 }
