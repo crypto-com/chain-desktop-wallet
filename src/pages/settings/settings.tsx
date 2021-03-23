69ae513a88f1604c89ee9ccb8f9473e021ac4cbd
crypto-matto <matthew.to@crypto.com>
2021-03-23 12:22:54 +0800

    Delete storage function

--- src/pages/settings/settings.tsx
+++ src/pages/settings/settings.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 import './settings.less';
 import 'antd/dist/antd.css';
-import { Button, Form, Input, Layout, Tabs } from 'antd';
+import { Button, Form, Input, Layout, Tabs, Alert } from 'antd';
 import { useRecoilState } from 'recoil';
 import { sessionState } from '../../recoil/atom';
 import { walletService } from '../../service/WalletService';
 import { NodeData } from '../../models/Wallet';
 import { Session } from '../../models/Session';
+import ModalPopup from '../../components/ModalPopup/ModalPopup';
 
 const { Header, Content, Footer } = Layout;
 const { TabPane } = Tabs;
@@ -69,10 +71,14 @@ const FormGeneral = () => {
 
 const FormSettings = () => {
   const [form] = Form.useForm();
+  const [confirmDeleteForm] = Form.useForm();
   const [isButtonLoading, setIsButtonLoading] = useState(false);
+  const [isConfirmationModalVisible, setIsConfirmationModalVisible] = useState(false);
+  const [isConfirmDeleteVisible, setIsConfirmDeleteVisible] = useState(false);
   const [session, setSession] = useRecoilState(sessionState);
   const defaultSettings = session.wallet.config;
   const didMountRef = useRef(false);
+  const history = useHistory();
 
   useEffect(() => {
     if (!didMountRef.current) {
@@ -118,6 +124,18 @@ const FormSettings = () => {
     });
   };
 
+  const handleCancelConfirmationModal = () => {
+    setIsConfirmationModalVisible(false);
+    setIsConfirmDeleteVisible(false);
+  };
+
+  const onConfirmDelete = () => {
+    setIsConfirmationModalVisible(false);
+    indexedDB.deleteDatabase('NeDB');
+    history.replace('/');
+    history.go(0);
+  };
+
   return (
     <Form
       {...layout}
@@ -143,6 +161,104 @@ const FormSettings = () => {
               </Form.Item>
             </div>
           </div>
+        </TabPane>
+        <TabPane tab="Delete Storage" key="2">
+          <div className="site-layout-background settings-content">
+            <div className="container">
+              <div className="description">
+                Once you delete the storage, you will lose access to all you wallets. The only way
+                to regain wallet access is by restoring wallet recovery mnemonic phrase. <br />
+              </div>
+              <Button
+                type="primary"
+                loading={isButtonLoading}
+                onClick={() => setIsConfirmationModalVisible(true)}
+              >
+                Remove
+              </Button>
+            </div>
+          </div>
+          <ModalPopup
+            isModalVisible={isConfirmationModalVisible}
+            handleCancel={handleCancelConfirmationModal}
+            handleOk={onConfirmDelete}
+            confirmationLoading={isButtonLoading}
+            footer={[
+              <Button
+                key="submit"
+                type="primary"
+                loading={isButtonLoading}
+                onClick={() => setIsConfirmDeleteVisible(true)}
+                hidden={isConfirmDeleteVisible}
+              >
+                Confirm
+              </Button>,
+              <Button
+                key="back"
+                type="link"
+                onClick={handleCancelConfirmationModal}
+                hidden={isConfirmDeleteVisible}
+              >
+                Cancel
+              </Button>,
+            ]}
+            okText="Confirm"
+          >
+            <>
+              <div className="title">Confirm Delete Storage</div>
+              <div className="description">
+                You may wish to verify your recovery mnemonic phrase before deletion to ensure that
+                you can restore this wallet in the future.
+              </div>
+              {/* <div className="item">
+                <div className="label">Sender Address</div>
+              </div>
+              <div className="item">
+                <div className="label">Undelegate From Validator</div>
+              </div> */}
+              <div>
+                <Alert
+                  type="warning"
+                  message="Are you sure you want to delete the storage? If you have not backed up your mnemonic phrase, you will result in losing your funds forever."
+                  showIcon
+                />
+              </div>
+              {isConfirmDeleteVisible ? (
+                <Form
+                  {...layout}
+                  layout="vertical"
+                  form={confirmDeleteForm}
+                  name="control-hooks"
+                  requiredMark="optional"
+                  onFinish={onConfirmDelete}
+                >
+                  <Form.Item
+                    name="delete"
+                    label="Please enter DELETE"
+                    hasFeedback
+                    rules={[
+                      {
+                        required: true,
+                      },
+                      {
+                        pattern: /DELETE/,
+                        message: 'Please enter DELETE',
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                  <Form.Item {...tailLayout} className="button">
+                    <Button type="primary" htmlType="submit" loading={isButtonLoading}>
+                      Delete Storage
+                    </Button>
+                  </Form.Item>
+                </Form>
+              ) : (
+                ''
+              )}
+            </>
+          </ModalPopup>
         </TabPane>
       </Tabs>
     </Form>
