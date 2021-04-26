601a5f9023a9825a023563051bb65c08fbd62f12
wildera <46605957+wildera@users.noreply.github.com>
2021-04-26 14:35:13 +0200

    funct to format number to a more readable string
    numeral has some weird rounding issues when formatting number with a high decimal count, so let's make our own

--- src/utils/NumberUtils.ts
+++ src/utils/NumberUtils.ts
@@ -114,3 +114,18 @@ export function adjustedTransactionAmount(
   }
   return formAmount;
 }
+
+export function getFormattedAmount(amount: string): string {
+  // replace hardcoded separators if/when localization is available
+  const decimalSeparator = '.';
+  const thousandSeparator = ',';
+
+  const elements = amount.split(decimalSeparator);
+  let integers = elements[0];
+  const decimals = elements.length > 1 ? decimalSeparator + elements[1].replace(/[0]+$/, '') : '';
+  const rgxThousand = /(\d+)(\d{3})/;
+  while (rgxThousand.test(integers)) {
+    integers = integers.replace(rgxThousand, `$1${thousandSeparator}$2`);
+  }
+  return integers + decimals;
+}
