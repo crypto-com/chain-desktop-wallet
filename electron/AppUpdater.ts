efedb3ef653ca0263c4cc7a56895d5873598d5d3
Eddy CPC <eddy.wanny@crypto.com>
2021-07-05 13:05:04 +0300

    Added handling for auto updater

--- electron/AppUpdater.ts
+++ electron/AppUpdater.ts
@@ -1,10 +1,12 @@
-import { autoUpdater } from "electron-updater"
-
-export default class AppUpdater {
-    constructor() {
-        const log = require("electron-log")
-        log.transports.file.level = "debug"
-        autoUpdater.logger = log
-        autoUpdater.checkForUpdatesAndNotify()
-    }
-}
+// import { autoUpdater } from "electron-updater"
+//
+// export default class AppUpdater {
+//     constructor() {
+//         const log = require("electron-log")
+//         log.transports.file.level = "debug"
+//         autoUpdater.logger = log
+//         const updaterResult = autoUpdater.checkForUpdatesAndNotify()
+//
+//         console.log('updaterResult', updaterResult)
+//     }
+// }
