961fd97a59ad979f988a64edb91e79c073973b7e
crypto-matto <matthew.to@crypto.com>
2021-07-21 16:34:52 +0800

    A proper implementation on languageDetector

--- src/language/I18n.ts
+++ src/language/I18n.ts
@@ -5,26 +5,20 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { generalConfigService } from '../storage/GeneralConfigService';
 import translations from './translations.json';
 
-let savedLanguage: string = '';
-
-const getLanguageFromDB = async () => {
-  const language = await generalConfigService.getLanguage();
-  savedLanguage = language;
-};
-
-getLanguageFromDB();
+const languageDetector = new LanguageDetector(async () => {
+  return await generalConfigService.getLanguage();
+});
 
 i18n
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
+  .use(languageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    lng: savedLanguage,
     fallbackLng: 'enUS',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
