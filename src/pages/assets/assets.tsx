319cd904c1c3a9479c7d4e7cea21c56302e7d055
crypto-matto <matthew.to@crypto.com>
2021-08-24 18:24:18 +0800

    Added new icons for tab

--- src/pages/assets/assets.tsx
+++ src/pages/assets/assets.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next';
 import './assets.less';
 import 'antd/dist/antd.css';
 import { Layout, Table, Avatar, Tabs, Tag, Typography, Dropdown, Menu } from 'antd';
-import { ArrowLeftOutlined, MoreOutlined } from '@ant-design/icons';
+import Icon, { ArrowLeftOutlined, MoreOutlined } from '@ant-design/icons';
 import {
   sessionState,
   marketState,
@@ -30,6 +30,8 @@ import {
   TransactionStatus,
   TransferTransactionData,
 } from '../../models/Transaction';
+import IconSend from '../../svg/IconSend';
+import IconReceive from '../../svg/IconReceive';
 
 const { Header, Content, Footer } = Layout;
 const { TabPane } = Tabs;
@@ -91,6 +93,7 @@ const AssetsPage = () => {
   // const [isLedger, setIsLedger] = useState(false);
   const [currentAsset, setCurrentAsset] = useState<UserAsset | undefined>(session.activeAsset);
   const [isAssetVisible, setIsAssetVisible] = useState(false);
+  const [activeAssetTab, setActiveAssetTab] = useState('1');
   const [allTransfer, setAllTransfer] = useState<any>();
 
   // const [defaultWalletAsset, setdefaultWalletAsset] = useState<UserAsset>();
@@ -338,6 +341,7 @@ const AssetsPage = () => {
 
                   <Tabs
                     defaultActiveKey={getDefaultAssetTab(locationState, navbarMenuSelectedKey)}
+                    activeKey={activeAssetTab}
                     onTabClick={key => {
                       if (key === '2') {
                         setNavbarMenuSelectedKey('/send');
@@ -347,6 +351,44 @@ const AssetsPage = () => {
                       }
                     }}
                     centered
+                    renderTabBar={() => {
+                      // renderTabBar={(props) => {
+                      return (
+                        <div className="tab-container">
+                          <div onClick={() => setActiveAssetTab('2')}>
+                            <>
+                              <Icon
+                                className={`tab ${activeAssetTab === '2' ? 'active' : ''}`}
+                                component={IconSend}
+                              />
+                              {t('navbar.send')}
+                            </>
+                          </div>
+                          <div onClick={() => setActiveAssetTab('3')}>
+                            <>
+                              <Icon
+                                className={`tab ${activeAssetTab === '3' ? 'active' : ''}`}
+                                component={IconReceive}
+                              />
+                              {t('navbar.receive')}
+                            </>
+                          </div>
+                          {/* {props.panes.map((tab) => {
+                          return <div onClick={() => setActiveAssetTab(tab.key)}>
+                            {tab.key === '2'
+                              ? <>
+                                <Icon className="tab" component={IconSend} />
+                                {tab.props.tab}
+                              </>
+                              : <>
+                                <Icon className="tab" component={IconReceive} />
+                                {tab.props.tab}
+                              </>}
+                          </div>
+                        })} */}
+                        </div>
+                      );
+                    }}
                   >
                     <TabPane tab={t('assets.tab1')} key="1">
                       <Table
