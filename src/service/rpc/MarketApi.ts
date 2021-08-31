122a12c02e721c9bfdecff3a9b22c2756a493526
cdc-Hitesh <hitesh.goel@crypto.com>
2021-09-01 01:29:56 +0530

    Keep a single source of coin rate

--- src/service/rpc/MarketApi.ts
+++ src/service/rpc/MarketApi.ts
@@ -1,7 +1,5 @@
-/*eslint-disable no-else-return*/
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import { AssetMarketPrice } from '../../models/UserAsset';
-import { CroPrice } from './CroMarketApiModels';
 import { MARKET_API_BASE_URL, COINBASE_TICKER_API_BASE_URL } from '../../config/StaticConfig';
 
 export interface IMarketApi {
@@ -30,22 +28,6 @@ export class CroMarketApi implements IMarketApi {
   }
 
   public async getAssetPrice(assetSymbol: string, currency: string): Promise<AssetMarketPrice> {
-
-    if (assetSymbol.toLowerCase() === 'cro' && currency.toLowerCase() === 'usd') {
-      const croMarketPrice = await this.axiosClient.get<CroPrice>('/coins/show');
-      const loadedSymbol = croMarketPrice.data.coin.symbol;
-      const loadedCurrency = croMarketPrice.data.coin.price_native.currency;
-      if (assetSymbol.toUpperCase() !== loadedSymbol || loadedCurrency !== currency.toUpperCase()) {
-        throw TypeError('Could not find requested market price info');
-      }
-
-      return {
-        assetSymbol: loadedSymbol,
-        currency: loadedCurrency,
-        dailyChange: croMarketPrice.data.coin.percent_change_native_24h,
-        price: croMarketPrice.data.coin.price_native.amount,
-      };
-    }
 
     const fiatPrice = await this.getCryptoToFiatRateFromCoinbase(assetSymbol, currency);
     return {
