0969031c99faee54a4a229b28800fb2b853d61ae
crypto-matto <matthew.to@crypto.com>
2021-09-09 18:51:40 +0800

    Add explorer in config update

--- src/pages/settings/settings.tsx
+++ src/pages/settings/settings.tsx
@@ -745,6 +745,7 @@ const FormSettings = () => {
       indexingUrl: values.indexingUrl,
       networkFee: String(values.networkFee),
       gasLimit: String(values.gasLimit),
+      explorer: session.wallet.config.explorer,
     };
 
     // This wallet level settings update should only imply the primary asset.
@@ -760,7 +761,7 @@ const FormSettings = () => {
       ...session.activeAsset!,
       config: {
         ...previousAssetConfig!,
-        // explorerUrl: settingsDataUpdate.indexingUrl!,
+        explorer: settingsDataUpdate.explorer!,
         chainId: settingsDataUpdate.chainId!,
         fee: { gasLimit: settingsDataUpdate.gasLimit!, networkFee: settingsDataUpdate.networkFee! },
         indexingUrl: settingsDataUpdate.indexingUrl!,
