529e1d6fd68435e1577effa799537bea86c46828
crypto-matto <matthew.to@crypto.com>
2021-10-13 01:35:18 +0800

    Fix IBC transfer on Ledger device

--- src/service/signers/LedgerTransactionSigner.ts
+++ src/service/signers/LedgerTransactionSigner.ts
@@ -218,6 +218,11 @@ export class LedgerTransactionSigner extends BaseTransactionSigner implements IT
       sourceChannel: transaction.channel || '',
       sourcePort: transaction.port || '',
       timeoutTimestampInNanoSeconds: Long.fromValue(timeout),
+      timeoutHeight: {
+        // TODO: These numbers are hard-coded for now but we shall make it more dynamic later
+        revisionNumber: Long.fromString('122', true),
+        revisionHeight: Long.fromString('1708515', true),
+      },
       receiver: transaction.toAddress,
       token: new cro.Coin(transaction.amount, Units.BASE),
     });
