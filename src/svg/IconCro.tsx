8640dd7fc5c65bedec797ef5013f5be73901ec21
crypto-matto <matthew.to@crypto.com>
2021-10-28 17:02:59 +0800

    Add ledger app icon

---
+++ src/svg/IconCro.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+function IconCro(props) {
+  return (
+    <svg width={40} height={41} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M19.958 1.667L3.333 11.3V30.57l16.625 9.634 16.625-9.634V11.3L19.958 1.665zm11.647 26.068l-11.647 6.8-11.694-6.8V14.181l11.694-6.8 11.694 6.8v13.554h-.047z"
+        fill="currentColor"
+      />
+      <path
+        d="M19.958 7.38V1.668L3.333 11.3V30.57l16.625 9.634v-5.668l-11.694-6.8V14.181l11.694-6.8z"
+        fill="currentColor"
+      />
+      <path
+        d="M19.958 34.535v5.715l16.625-9.634V11.3L19.958 1.666v5.715l11.694 6.8v13.554l-11.694 6.8z"
+        fill="currentColor"
+      />
+      <path
+        d="M27.707 25.466l-7.749 4.487-7.796-4.486v-9.02l7.796-4.487 7.749 4.486v9.02z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+export default IconCro;
