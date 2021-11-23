f5b0064ad294f66477c69a57a77a95ea42eb48fb
XinyuCRO <xinyu.zhao@crypto.com>
2021-11-23 16:05:19 +0800

    refactor: move listeners to useIPCProvider hooks

--- src/pages/settings/tabs/DappBrowser/DappBrowser.tsx
+++ src/pages/settings/tabs/DappBrowser/DappBrowser.tsx
@@ -1,27 +1,15 @@
 import * as React from 'react';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { WebviewTag } from 'electron';
 // import { Modal } from 'antd';
 import { useRecoilValue } from 'recoil';
 import { useTranslation } from 'react-i18next';
-import { TransactionConfig } from 'web3-eth';
-import { ProviderPreloadScriptPath } from './useIPCProvider';
+import Web3 from 'web3';
+import { ProviderPreloadScriptPath, useIPCProvider } from './useIPCProvider';
 import { sessionState, walletAllAssetsState } from '../../../../recoil/atom';
 import { getCronosAsset } from '../../../../utils/utils';
 import PasswordFormModal from '../../../../components/PasswordForm/PasswordFormModal';
 import { secretStoreService } from '../../../../storage/SecretStoreService';
-import { evmTransactionSigner } from '../../../../service/signers/EvmTransactionSigner';
-import { EVMContractCallUnsigned } from '../../../../service/signers/TransactionSupported';
-import { TransactionPrepareService } from '../../../../service/TransactionPrepareService';
-import { walletService } from '../../../../service/WalletService';
-import { ChainConfig } from './config';
-import { DappBrowserIPC } from './types';
-
-const useRefEventListener = (fn: Function) => {
-  const fnRef = useRef(fn);
-  fnRef.current = fn;
-  return fnRef;
-};
 
 const DappBrowser = () => {
   const webviewRef = useRef<WebviewTag & HTMLWebViewElement>(null);
@@ -30,220 +18,65 @@ const DappBrowser = () => {
   const cronosAsset = getCronosAsset(allAssets);
 
   const [inputPasswordVisible, setInputPasswordVisible] = useState(false);
-  const [passphrase, setPassphrase] = useState('');
   const currentSession = useRecoilValue(sessionState);
+  const [confirmPasswordCallback, setConfirmPasswordCallback] = useState<{
+    successCallback: Function;
+    errorCallback: Function;
+  }>();
 
-  const transactionPrepareService = new TransactionPrepareService(walletService.storageService);
-
-  useEffect(() => {
-    setInputPasswordVisible(true);
-  }, []);
-
-  const executeJavScript = (script: string) => {
-    webviewRef.current?.executeJavaScript(
-      `
-        (function() {
-            ${script}
-        })();
-        `,
-    );
+  const signMessage = (
+    data: string,
+    addPrefix: boolean,
+    successHandler: { (data: string): void },
+    errorHandler: { (reason: string): void },
+  ) => {
+    // TODO: use privateKey to sign data
+    console.log(data, addPrefix, successHandler, errorHandler);
+    return data;
   };
 
-  // const sendError = (id: number, error: string) => {
-  //   executeJavScript(`
-  //       window.ethereum.sendError(${id}, "${error}")
-  //   `);
-  // };
-
-  const sendResponse = (id: number, response: string) => {
-    executeJavScript(`
-        window.ethereum.sendResponse(${id}, "${response}")
-    `);
-  };
-
-  // const signMessage = (
-  //   data: string,
-  //   addPrefix: boolean,
-  //   successHandler: { (data: string): void },
-  //   errorHandler: { (reason: string): void },
-  // ) => {
-  //   // TODO: use privateKey to sign data
-  //   console.log(data, addPrefix, successHandler, errorHandler);
-  //   return data;
-  // };
-
-  const onSignTXRef = useRefEventListener(async (event: DappBrowserIPC.SendTransactionEvent) => {
-    const prepareTXConfig: TransactionConfig = {
-      from: event.object.from,
-      to: event.object.to,
-    };
-
-    const prepareTxInfo = await transactionPrepareService.prepareEVMTransaction(
-      cronosAsset!,
-      prepareTXConfig,
-    );
-
-    const txConfig: EVMContractCallUnsigned = {
-      from: event.object.from,
-      contractAddress: event.object.to,
-      data: event.object.data,
-      gasLimit: String(event.object.gas),
-      gasPrice: event.object.gasPrice,
-      nonce: prepareTxInfo.nonce,
-    };
-    const result = await evmTransactionSigner.signTransaction(txConfig, passphrase);
-
-    sendResponse(event.id, result);
+  useIPCProvider({
+    webview: webviewRef.current,
+    onRequestAddress: async successHandler => {
+      // const modal = Modal.confirm({
+      //   title: 'Requesting Address',
+      //   content: 'Approve or not?',
+      //   onOk: () => {
+      successHandler(cronosAsset?.address!);
+      // modal.destroy();
+      //   },
+      //   onCancel: () => {
+      //     errorHandler('Canceled');
+      //     modal.destroy();
+      //   },
+      // });
+    },
+    onSignMessage: async (event, successHandler, errorHandler) => {
+      signMessage(event.object.data, false, successHandler, errorHandler);
+    },
+    onSignPersonalMessage: async (event, successHandler, errorHandler) => {
+      signMessage(event.object.data, true, successHandler, errorHandler);
+    },
+    onSignTypedMessage: async (event, successHandler, errorHandler) => {
+      signMessage(event.object.data, true, successHandler, errorHandler);
+    },
+    onEcRecover: async (event, successHandler, errorHandler) => {
+      new Web3('').eth.personal
+        .ecRecover(event.object.message, event.object.signature)
+        .then(errorHandler, successHandler);
+    },
+    onRequestSendTransaction: async (event, successCallback, errorCallback) => {
+      // prompt for password
+      setInputPasswordVisible(true);
+      setConfirmPasswordCallback({ successCallback, errorCallback });
+    },
+    onAddEthereumChain: async () => {
+      // no-op, cause we only support cronos for now
+    },
+    onWatchAsset: async () => {
+      // no-op for now
+    },
   });
-
-  const sendResponses = (id: number, responses: string[]) => {
-    const script = responses.map(r => `'${r}'`).join(',');
-    executeJavScript(`
-        window.ethereum.sendResponse(${id}, [${script}])
-    `);
-  };
-
-  const handleRequestAccountsRef = useRefEventListener((id: number) => {
-    if (!cronosAsset?.address) {
-      return;
-    }
-    const { address } = cronosAsset;
-    executeJavScript(
-      `
-          window.ethereum.setAddress("${address}");
-        `,
-    );
-    sendResponses(id, [address]);
-  });
-
-  const injectDomReadyScript = () => {
-    if (!webviewRef.current) {
-      return;
-    }
-    executeJavScript(
-      `
-            var config = {
-                chainId: ${ChainConfig.chainId},
-                rpcUrl: "${ChainConfig.rpcUrl}",
-                isDebug: true
-            };
-            window.ethereum = new window.desktopWallet.Provider(config);
-            window.web3 = new window.desktopWallet.Web3(window.ethereum);
-            console.log(window.ethereum);
-        `,
-    );
-  };
-
-  const listenIPCMessages = () => {
-    if (!webviewRef.current) {
-      return;
-    }
-
-    webviewRef.current.addEventListener('ipc-message', async e => {
-      const { channel, args } = e;
-
-      if (channel !== DappBrowserIPC.ChannelName) {
-        return;
-      }
-
-      if (args?.length < 1) {
-        return;
-      }
-
-      const event: DappBrowserIPC.Event = args[0];
-
-      switch (event.name) {
-        case 'requestAccounts':
-          handleRequestAccountsRef.current(event.id);
-          break;
-        case 'signTransaction':
-          onSignTXRef.current(event);
-          break;
-        case 'signMessage':
-          break;
-        case 'signPersonalMessage':
-          break;
-        case 'signTypedMessage':
-          break;
-        case 'ecRecover':
-          break;
-        case 'watchAsset':
-          break;
-        case 'addEthereumChain':
-          break;
-        default:
-          break;
-      }
-    });
-  };
-
-  const setupIPC = () => {
-    if (!webviewRef.current) {
-      return;
-    }
-
-    const webview = webviewRef.current;
-
-    listenIPCMessages();
-
-    webview.addEventListener('dom-ready', () => {
-      // TODO: remove later
-      injectDomReadyScript();
-      webview.openDevTools();
-    });
-  };
-
-  useEffect(() => {
-    setupIPC();
-  }, [webviewRef]);
-
-  // useWebviewIPCProvider({
-  //   webview: webviewRef.current,
-  //   onRequestAddress: async successHandler => {
-  //     // const modal = Modal.confirm({
-  //     //   title: 'Requesting Address',
-  //     //   content: 'Approve or not?',
-  //     //   onOk: () => {
-  //     successHandler(cronosAsset?.address!);
-  //     // modal.destroy();
-  //     //   },
-  //     //   onCancel: () => {
-  //     //     errorHandler('Canceled');
-  //     //     modal.destroy();
-  //     //   },
-  //     // });
-  //   },
-  //   onSignMessage: async (event, successHandler, errorHandler) => {
-  //     signMessage(event.object.data, false, successHandler, errorHandler);
-  //   },
-  //   onSignPersonalMessage: async (event, successHandler, errorHandler) => {
-  //     signMessage(event.object.data, true, successHandler, errorHandler);
-  //   },
-  //   onSignTypedMessage: async (event, successHandler, errorHandler) => {
-  //     signMessage(event.object.data, true, successHandler, errorHandler);
-  //   },
-  //   onEcRecover: async (event, successHandler, errorHandler) => {
-  //     new Web3('').eth.personal
-  //       .ecRecover(event.object.message, event.object.signature)
-  //       .then(errorHandler, successHandler);
-  //   },
-  //   onSignTransaction: onSignTX,
-  //   onAddEthereumChain: async () => {
-  //     // no-op, cause we only support cronos for now
-  //   },
-  //   onWatchAsset: async () => {
-  //     // no-op for now
-  //   },
-  // });
-
-  const onSuccessInputPassword = async (password: string) => {
-    const phraseDecrypted = await secretStoreService.decryptPhrase(
-      password,
-      currentSession.wallet.identifier,
-    );
-    setPassphrase(phraseDecrypted);
-    setInputPasswordVisible(false);
-  };
 
   return (
     <div className="site-layout-background settings-content">
@@ -253,8 +86,18 @@ const DappBrowser = () => {
           okButtonText={t('general.passwordFormModal.okButton')}
           onCancel={() => {
             setInputPasswordVisible(false);
+            confirmPasswordCallback?.errorCallback('Canceled');
+            setConfirmPasswordCallback(undefined);
           }}
-          onSuccess={onSuccessInputPassword}
+          onSuccess={async (password: string) => {
+            const phraseDecrypted = await secretStoreService.decryptPhrase(
+              password,
+              currentSession.wallet.identifier,
+            );
+            setInputPasswordVisible(false);
+            confirmPasswordCallback?.successCallback(phraseDecrypted);
+            setConfirmPasswordCallback(undefined);
+          }}
           onValidatePassword={async (password: string) => {
             const isValid = await secretStoreService.checkIfPasswordIsValid(password);
             return {
--- src/pages/settings/tabs/DappBrowser/useIPCProvider.ts
+++ src/pages/settings/tabs/DappBrowser/useIPCProvider.ts
@@ -1,55 +1,234 @@
-// import { WebviewTag } from 'electron';
-// import { useEffect } from 'react';
-// import { ChainConfig } from './config';
-// import { DappBrowserIPC } from './types';
+import { WebviewTag } from 'electron';
+import { useCallback, useEffect, useRef } from 'react';
+import { TransactionConfig } from 'web3-eth';
+import { useRecoilValue } from 'recoil';
+import { TransactionPrepareService } from '../../../../service/TransactionPrepareService';
+import { walletService } from '../../../../service/WalletService';
+import { ChainConfig } from './config';
+import { DappBrowserIPC } from './types';
+import { evmTransactionSigner } from '../../../../service/signers/EvmTransactionSigner';
+import { EVMContractCallUnsigned } from '../../../../service/signers/TransactionSupported';
+import { walletAllAssetsState } from '../../../../recoil/atom';
+import { getCronosAsset } from '../../../../utils/utils';
 
 export const ProviderPreloadScriptPath =
   'file:///Users/xinyu/Developer/cro/chain-desktop-wallet/src/pages/settings/tabs/DappBrowser/preload.js';
 
-// type WebView = WebviewTag & HTMLWebViewElement;
+type WebView = WebviewTag & HTMLWebViewElement;
 
-// type ErrorHandler = (reason: string) => void;
+type ErrorHandler = (reason: string) => void;
 
-// interface IUseIPCProviderProps {
-//   webview: WebView | null;
-//   onRequestAddress: (onSuccess: (address: string) => void, onError: ErrorHandler) => Promise<void>;
-//   onSignTransaction: (
-//     event: DappBrowserIPC.SendTransactionEvent,
-//     onSuccess: (signedTx: string) => void,
-//     onError: ErrorHandler,
-//   ) => Promise<void>;
-//   onSignMessage: (
-//     event: DappBrowserIPC.SignMessageEvent,
-//     onSuccess: (signedMessage: string) => void,
-//     onError: ErrorHandler,
-//   ) => Promise<void>;
-//   onSignPersonalMessage: (
-//     event: DappBrowserIPC.SignPersonalMessageEvent,
-//     onSuccess: (signedMessage: string) => void,
-//     onError: ErrorHandler,
-//   ) => Promise<void>;
-//   onSignTypedMessage: (
-//     event: DappBrowserIPC.SignTypedMessageEvent,
-//     onSuccess: (signedMessage: string) => void,
-//     onError: ErrorHandler,
-//   ) => Promise<void>;
-//   onEcRecover: (
-//     event: DappBrowserIPC.EcrecoverEvent,
-//     onSuccess: (address: string) => void,
-//     onError: ErrorHandler,
-//   ) => Promise<void>;
-//   onWatchAsset: (
-//     event: DappBrowserIPC.WatchAssetEvent,
-//     onSuccess: () => void,
-//     onError: ErrorHandler,
-//   ) => Promise<void>;
-//   onAddEthereumChain: (
-//     event: DappBrowserIPC.AddEthereumChainEvent,
-//     onSuccess: () => void,
-//     onError: ErrorHandler,
-//   ) => Promise<void>;
-// }
+interface IUseIPCProviderProps {
+  webview: WebView | null;
+  onRequestAddress: (onSuccess: (address: string) => void, onError: ErrorHandler) => Promise<void>;
+  onRequestSendTransaction: (
+    event: DappBrowserIPC.SendTransactionEvent,
+    onSuccess: (passphrase: string) => void,
+    onError: ErrorHandler,
+  ) => Promise<void>;
+  onSignMessage: (
+    event: DappBrowserIPC.SignMessageEvent,
+    onSuccess: (signedMessage: string) => void,
+    onError: ErrorHandler,
+  ) => Promise<void>;
+  onSignPersonalMessage: (
+    event: DappBrowserIPC.SignPersonalMessageEvent,
+    onSuccess: (signedMessage: string) => void,
+    onError: ErrorHandler,
+  ) => Promise<void>;
+  onSignTypedMessage: (
+    event: DappBrowserIPC.SignTypedMessageEvent,
+    onSuccess: (signedMessage: string) => void,
+    onError: ErrorHandler,
+  ) => Promise<void>;
+  onEcRecover: (
+    event: DappBrowserIPC.EcrecoverEvent,
+    onSuccess: (address: string) => void,
+    onError: ErrorHandler,
+  ) => Promise<void>;
+  onWatchAsset: (
+    event: DappBrowserIPC.WatchAssetEvent,
+    onSuccess: () => void,
+    onError: ErrorHandler,
+  ) => Promise<void>;
+  onAddEthereumChain: (
+    event: DappBrowserIPC.AddEthereumChainEvent,
+    onSuccess: () => void,
+    onError: ErrorHandler,
+  ) => Promise<void>;
+}
 
-// export const useWebviewIPCProvider = (props: IUseIPCProviderProps) => {
+const useRefCallback = (fn: Function) => {
+  const fnRef = useRef(fn);
+  fnRef.current = fn;
+  return fnRef;
+};
 
-// };
+export const useIPCProvider = (props: IUseIPCProviderProps) => {
+  const { webview } = props;
+
+  const transactionPrepareService = new TransactionPrepareService(walletService.storageService);
+  const allAssets = useRecoilValue(walletAllAssetsState);
+  const cronosAsset = getCronosAsset(allAssets);
+
+  const executeJavScript = useCallback(
+    (script: string) => {
+      webview?.executeJavaScript(
+        `
+        (function() {
+            ${script}
+        })();
+        `,
+      );
+    },
+    [webview],
+  );
+
+  const injectDomReadyScript = useCallback(() => {
+    if (!webview) {
+      return;
+    }
+    executeJavScript(
+      `
+            var config = {
+                chainId: ${ChainConfig.chainId},
+                rpcUrl: "${ChainConfig.rpcUrl}",
+                isDebug: true
+            };
+            window.ethereum = new window.desktopWallet.Provider(config);
+            window.web3 = new window.desktopWallet.Web3(window.ethereum);
+            console.log(window.ethereum);
+        `,
+    );
+  }, [webview]);
+
+  const sendError = (id: number, error: string) => {
+    executeJavScript(`
+        window.ethereum.sendError(${id}, "${error}")
+    `);
+  };
+
+  const sendResponse = (id: number, response: string) => {
+    executeJavScript(`
+        window.ethereum.sendResponse(${id}, "${response}")
+    `);
+  };
+
+  const sendResponses = (id: number, responses: string[]) => {
+    const script = responses.map(r => `'${r}'`).join(',');
+    executeJavScript(`
+        window.ethereum.sendResponse(${id}, [${script}])
+    `);
+  };
+
+  const handleRequestAccounts = useRefCallback((id: number, address: string) => {
+    executeJavScript(
+      `
+          window.ethereum.setAddress("${address}");
+        `,
+    );
+    sendResponses(id, [address]);
+  });
+
+  const handleSendTransaction = useRefCallback(
+    async (event: DappBrowserIPC.SendTransactionEvent, passphrase: string) => {
+      const prepareTXConfig: TransactionConfig = {
+        from: event.object.from,
+        to: event.object.to,
+      };
+
+      const prepareTxInfo = await transactionPrepareService.prepareEVMTransaction(
+        cronosAsset!,
+        prepareTXConfig,
+      );
+
+      const txConfig: EVMContractCallUnsigned = {
+        from: event.object.from,
+        contractAddress: event.object.to,
+        data: event.object.data,
+        gasLimit: String(event.object.gas),
+        gasPrice: event.object.gasPrice,
+        nonce: prepareTxInfo.nonce,
+      };
+      const result = await evmTransactionSigner.signTransaction(txConfig, passphrase);
+
+      sendResponse(event.id, result);
+    },
+  );
+
+  const listenIPCMessages = () => {
+    if (!webview) {
+      return;
+    }
+
+    webview.addEventListener('ipc-message', async e => {
+      const { channel, args } = e;
+
+      if (channel !== DappBrowserIPC.ChannelName) {
+        return;
+      }
+
+      if (args?.length < 1) {
+        return;
+      }
+
+      const event: DappBrowserIPC.Event = args[0];
+
+      switch (event.name) {
+        case 'requestAccounts':
+          props.onRequestAddress(
+            address => {
+              handleRequestAccounts.current(event.id, address);
+            },
+            reason => {
+              sendError(event.id, reason);
+            },
+          );
+          break;
+        case 'signTransaction':
+          props.onRequestSendTransaction(
+            event,
+            passphrase => {
+              handleSendTransaction.current(event, passphrase);
+            },
+            reason => {
+              sendError(event.id, reason);
+            },
+          );
+          break;
+        case 'signMessage':
+          break;
+        case 'signPersonalMessage':
+          break;
+        case 'signTypedMessage':
+          break;
+        case 'ecRecover':
+          break;
+        case 'watchAsset':
+          break;
+        case 'addEthereumChain':
+          break;
+        default:
+          break;
+      }
+    });
+  };
+
+  const setupIPC = () => {
+    if (!webview) {
+      return;
+    }
+
+    listenIPCMessages();
+
+    webview.addEventListener('dom-ready', () => {
+      // TODO: remove later
+      injectDomReadyScript();
+      webview.openDevTools();
+    });
+  };
+
+  useEffect(() => {
+    setupIPC();
+  }, [webview]);
+};
