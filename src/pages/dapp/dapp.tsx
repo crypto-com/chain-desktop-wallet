547d0d677305eb138bd093971f6752e41ec082c7
XinyuCRO <xinyu.zhao@crypto.com>
2022-02-18 10:38:03 +0800

    feat: close dapp with quitting the page

--- src/pages/dapp/dapp.tsx
+++ src/pages/dapp/dapp.tsx
@@ -151,6 +151,7 @@ const DappPage = () => {
           isRefreshButtonDisabled: webviewNavigationState?.canRefresh === false,
           isBookmarkButtonDisabled: false,
           isBookmarkButtonHighlighted: bookmarkButtonHighlighted,
+          isExitButtonDisabled: shouldShowBrowser === false,
         }}
         buttonCallbacks={{
           onBackButtonClick: () => {
@@ -181,6 +182,17 @@ const DappPage = () => {
               faviconURL: bookMarkInfo.faviconURL,
             });
           },
+          onExitButtonClick: () => {
+            setSelectedDapp(undefined);
+            setSelectedURL('');
+            setAddressBarValue('');
+            setWebviewState('idle');
+            setWebviewNavigationState({
+              canGoBack: false,
+              canGoForward: false,
+              canRefresh: false,
+            });
+          },
         }}
         onSearch={value => {
           setSelectedDapp(undefined);
@@ -194,7 +206,7 @@ const DappPage = () => {
           }
         }}
       />
-      {shouldShowBrowser ? (
+      {shouldShowBrowser && (
         <DappBrowser
           dapp={selectedDapp}
           dappURL={selectedURL}
@@ -211,70 +223,73 @@ const DappPage = () => {
             updateBookmarkButtonBeHighlighted();
           }}
         />
-      ) : (
-        <>
-          <Header className="site-layout-background">{t('dapp.title')}</Header>
-          <div className="header-description">{t('dapp.description')}</div>
-          <Content>
-            <Tabs
-              defaultActiveKey={selectedTabKey}
-              onChange={value => {
-                setSelectedTabKey(value);
-              }}
-            >
-              <TabPane tab={t('dapp.tab.popular.title')} key={TabKey.popular}>
-                <div className="dapps">
-                  <div className="cards">
-                    {DappList.map((dapp, idx) => {
-                      return (
-                        <BorderlessCard
-                          key={`partner-${idx}`}
-                          onClick={() => {
-                            if (shouldShowDisclaimer(dapp.url)) {
-                              setSelectedShowDisclaimerURL(dapp.url);
-                              return;
-                            }
-
-                            setSelectedDapp(dapp);
-                          }}
-                        >
-                          <div className="logo">
-                            <img src={dapp.logo} alt={dapp.alt} />
-                          </div>
-                          <div className="text">
-                            <h3>{dapp.name}</h3>
-                            <p>{dapp.description}</p>
-                          </div>
-                        </BorderlessCard>
-                      );
-                    })}
-                  </div>
-                </div>
-              </TabPane>
-              <TabPane tab="Cronos DApps" key={TabKey.cronosDapps}>
-                <CronosDAppsTab
-                  onClickDapp={dapp => {
-                    setSelectedURL(dapp.link);
-                  }}
-                />
-              </TabPane>
-              <TabPane tab={t('dapp.tab.saved.title')} key={TabKey.saved}>
-                <SavedTab
-                  onClick={bookmark => {
-                    if (shouldShowDisclaimer(bookmark.url)) {
-                      setSelectedShowDisclaimerURL(bookmark.url);
-                      return;
-                    }
-
-                    setSelectedDapp(undefined);
-                    setSelectedURL(bookmark.url);
-                  }}
-                />
-              </TabPane>
-            </Tabs>
-          </Content>
-        </>
       )}
+      <div
+        style={{
+          display: shouldShowBrowser ? 'none' : 'block',
+        }}
+      >
+        <Header className="site-layout-background">{t('dapp.title')}</Header>
+        <div className="header-description">{t('dapp.description')}</div>
+        <Content>
+          <Tabs
+            defaultActiveKey={selectedTabKey}
+            onChange={value => {
+              setSelectedTabKey(value);
+            }}
+          >
+            <TabPane tab={t('dapp.tab.popular.title')} key={TabKey.popular}>
+              <div className="dapps">
+                <div className="cards">
+                  {DappList.map((dapp, idx) => {
+                    return (
+                      <BorderlessCard
+                        key={`partner-${idx}`}
+                        onClick={() => {
+                          if (shouldShowDisclaimer(dapp.url)) {
+                            setSelectedShowDisclaimerURL(dapp.url);
+                            return;
+                          }
+
+                          setSelectedDapp(dapp);
+                        }}
+                      >
+                        <div className="logo">
+                          <img src={dapp.logo} alt={dapp.alt} />
+                        </div>
+                        <div className="text">
+                          <h3>{dapp.name}</h3>
+                          <p>{dapp.description}</p>
+                        </div>
+                      </BorderlessCard>
+                    );
+                  })}
+                </div>
+              </div>
+            </TabPane>
+            <TabPane tab="Cronos DApps" key={TabKey.cronosDapps}>
+              <CronosDAppsTab
+                onClickDapp={dapp => {
+                  setSelectedURL(dapp.link);
+                }}
+              />
+            </TabPane>
+            <TabPane tab={t('dapp.tab.saved.title')} key={TabKey.saved}>
+              <SavedTab
+                onClick={bookmark => {
+                  if (shouldShowDisclaimer(bookmark.url)) {
+                    setSelectedShowDisclaimerURL(bookmark.url);
+                    return;
+                  }
+
+                  setSelectedDapp(undefined);
+                  setSelectedURL(bookmark.url);
+                }}
+              />
+            </TabPane>
+          </Tabs>
+        </Content>
+      </div>
     </Layout>
   );
 };
