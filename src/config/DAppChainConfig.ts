a670e36b8f5297aa3774d92c06a327425808b09c
XinyuCRO <xinyu.zhao@crypto.com>
2022-03-22 11:09:54 +0800

    feat: remove ethereum from default chain configs

--- src/config/DAppChainConfig.ts
+++ src/config/DAppChainConfig.ts
@@ -1,11 +1,5 @@
 import { MainNetEvmConfig, TestNetEvmConfig } from './StaticAssets';
 
-export const DefaultChainConfigIds = ["0x19", "0x152", "0x01"].map(id => parseInt(id, 16));
-
-export const isChainDefaultConfig = (chainId: string) => {
-  return DefaultChainConfigIds.includes(parseInt(chainId, 16));
-}
-
 export const DAppDefaultChainConfigs = [
   {
     chainId: "0x19",
@@ -29,15 +23,21 @@ export const DAppDefaultChainConfigs = [
       symbol: "TCRO"
     }
   },
-  {
-    chainId: "0x01",
-    chainName: "Ethereum Mainnet",
-    blockExplorerUrls: ["https://etherscan.io/"],
-    rpcUrls: ["https://api.mycryptoapi.com/eth"],
-    nativeCurrency: {
-      decimals: 18,
-      name: "Ethereum",
-      symbol: "ETH"
-    }
-  }
+  // {
+  //   chainId: "0x01",
+  //   chainName: "Ethereum Mainnet",
+  //   blockExplorerUrls: ["https://etherscan.io/"],
+  //   rpcUrls: ["https://api.mycryptoapi.com/eth"],
+  //   nativeCurrency: {
+  //     decimals: 18,
+  //     name: "Ethereum",
+  //     symbol: "ETH"
+  //   }
+  // }
 ]
+
+export const DefaultChainConfigIds = DAppDefaultChainConfigs.map(config => parseInt(config.chainId, 16));
+
+export const isChainDefaultConfig = (chainId: string) => {
+  return DefaultChainConfigIds.includes(parseInt(chainId, 16));
+}
