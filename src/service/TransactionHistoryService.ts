f99928458c8d38d141bee6d675e69f3e391eebaa
crypto-matto <matthew.to@crypto.com>
2022-04-22 15:26:01 +0800

    Add ERC20 market price fetching from Crypto.com Price API

--- src/service/TransactionHistoryService.ts
+++ src/service/TransactionHistoryService.ts
@@ -588,10 +588,7 @@ export class TransactionHistoryService {
 
     await Promise.all(
       assets.map(async (asset: UserAsset) => {
-        const assetPrice = await croMarketPriceApi.getAssetPrice(
-          asset.mainnetSymbol,
-          currentSession.currency,
-        );
+        const assetPrice = await croMarketPriceApi.getAssetPrice(asset, currentSession.currency);
         await this.storageService.saveAssetMarketPrice(assetPrice);
       }),
     );
