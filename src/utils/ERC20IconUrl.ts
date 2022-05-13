6961a16ad0f07f9da8c8940364053cb1e76ecf7a
cdc-Hitesh <hitesh.goel@crypto.com>
2022-05-13 17:24:47 +0530

    Add ERC20 token icon fetching from Trust wallet

---
+++ src/utils/ERC20IconUrl.ts
@@ -0,0 +1,12 @@
+import Web3 from 'web3';
+const { isAddress, toChecksumAddress } = Web3.utils;
+
+const TRUSTWALLET_ASSETS_BASE_URI = 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/';
+
+export const getErc20IconUrlByContractAddrss = (contractAddress: string) => {
+    if (!isAddress(contractAddress)) {
+        return '';
+    }
+
+    return TRUSTWALLET_ASSETS_BASE_URI.concat(`${toChecksumAddress(contractAddress)}/logo.png`)
+}
