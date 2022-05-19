78d94485a7969ea24f0f5bb8860657b64b6892c7
XinyuCRO <xinyu.zhao@crypto.com>
2022-05-19 16:28:41 +0800

    feat: add ethgasstation servIce

--- src/service/Gas.ts
+++ src/service/Gas.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { ethers } from "ethers";
 import { getRecoil } from "recoil-nexus";
 import { FIXED_DEFAULT_FEE, FIXED_DEFAULT_GAS_LIMIT } from "../config/StaticConfig";
 import { sessionState } from "../recoil/atom";
@@ -14,5 +16,53 @@ export async function getCronosTendermintFeeConfig() {
   return {
     networkFee: chainConfig?.fee.networkFee ?? FIXED_DEFAULT_FEE,
     gasLimit: Number(chainConfig?.fee.gasLimit ?? FIXED_DEFAULT_GAS_LIMIT)
+  }
+}
+
+
+interface EthereumGasStepsInfoResponse {
+  fast: number
+  fastest: number
+  safeLow: number
+  average: number
+  block_time: number
+  blockNum: number
+  speed: number
+  safeLowWait: number
+  avgWait: number
+  fastWait: number
+  fastestWait: number
+}
+
+export interface EthereumGasStepInfo {
+  average: ethers.BigNumber; // wei
+  averageWait: number; //minutes
+
+  fast: ethers.BigNumber;
+  fastWait: number;
+
+  safeLow: ethers.BigNumber;
+  safeLowWait: number;
+}
+
+// https://docs.ethgasstation.info/gas-price
+export async function fetchEthereumGasSteps() {
+    const response  = await axios.get<EthereumGasStepsInfoResponse>(`https://ethgasstation.info/api/ethgasAPI.json`)
+    return response.data
+}
+
+export async function getEthereumGasSteps(): Promise<EthereumGasStepInfo | undefined> {
+  try {
+    const response = await fetchEthereumGasSteps();
+    return {
+      average: ethers.utils.parseUnits((response.average / 10).toString(), 'gwei'),
+      averageWait: response.avgWait,
+      fast: ethers.utils.parseUnits((response.fast / 10).toString(), 'gwei'),
+      fastWait: response.fastWait,
+      safeLow: ethers.utils.parseUnits((response.safeLow / 10).toString(), 'gwei'),
+      safeLowWait: response.safeLowWait,
+    }
+  } catch (error) {
+    console.error("Failed to fetch gas steps ", error);
   }
 }
