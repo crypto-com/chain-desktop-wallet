d8ddfcfbbfc85f13c737845627d12507626baadb
crypto-matto <matthew.to@crypto.com>
2022-06-20 14:41:08 +0800

    Remove About

--- src/pages/settings/tabs/Support/Support.tsx
+++ src/pages/settings/tabs/Support/Support.tsx
@@ -16,13 +16,18 @@ const Support = () => {
             target="_blank"
             rel="noreferrer"
           >
-            {t('settings.support.helpCenter.title')}
+            {t('general.learnMore')}
           </a>
           <Divider />
           <div className="title">{t('settings.support.feedbackForm.title')}</div>
           <div className="description">{t('settings.support.feedbackForm.description')}</div>
           <a href="https://crypto-com.typeform.com/to/Eb5daoBF" target="_blank" rel="noreferrer">
             {t('settings.support.feedbackForm.title')}
+          </a>
+          <Divider />
+          <div className="title">{t('settings.about.termsAndConditions.title')}</div>
+          <a href="https://crypto.com/document/desktop_wallet" target="_blank" rel="noreferrer">
+            {t('settings.about.termsAndConditions.title')}
           </a>
         </div>
       </div>
