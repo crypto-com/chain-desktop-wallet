555a3a9d3349cd4914640157fc4f3c80e12657ba
crypto-matto <matthew.to@crypto.com>
2022-09-08 17:23:26 +0800

    Use enum for RPCErrorCode

--- src/pages/dapp/browser/useIPCProvider.ts
+++ src/pages/dapp/browser/useIPCProvider.ts
@@ -21,7 +21,14 @@ import { useRefCallback } from '../../../hooks/useRefCallback';
 
 const remote = window.require('@electron/remote');
 
-type RPCErrorCode = 4001 | 4902
+enum RPCErrorCode {
+  USER_REJECTED_REQUEST = 4001,
+  UNAUTHORIZED = 4100,
+  UNSUPPORTED_METHOD = 4200,
+  DISCONNECTED = 4900,
+  CHAIN_DISCONNECTED = 4901,
+  REQUESTED_CHAIN_NOT_ADDED = 4902,
+} 
 
 interface RPCError {
   code: RPCErrorCode;
@@ -232,7 +239,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
 
         sendResponse(event.id, result);
       } catch (error) {
-        sendError(event.id, { code: 4001, message: 'Transaction failed' });
+        sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: 'Transaction failed' });
       }
 
       onFinishTransaction();
@@ -267,7 +274,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
         sendResponse(event.id, result);
         onFinishTransaction();
       } catch (error) {
-        sendError(event.id, { code: 4001, message: (error as any).toString() });
+        sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: (error as any).toString() });
         onFinishTransaction((error as any).toString());
       }
     },
@@ -279,7 +286,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
         const sig = await EvmTransactionSigner.signPersonalMessage(data, passphrase);
         sendResponse(eventId, sig);
       } catch (error) {
-        sendError(eventId, { code: 4001, message: (error as any).toString() });
+        sendError(eventId, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: (error as any).toString() });
       }
     },
   );
@@ -290,7 +297,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
         const sig = await EvmTransactionSigner.signTypedDataV4(event.object.raw, passphrase);
         sendResponse(event.id, sig);
       } catch (error) {
-        sendError(event.id, { code: 4001, message: (error as any).toString() });
+        sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: (error as any).toString() });
       }
     },
   );
@@ -320,7 +327,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
               handleRequestAccounts.current(event.id, address);
             },
             reason => {
-              sendError(event.id, { code: 4001, message: reason });
+              sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: reason });
             },
           );
           break;
@@ -368,7 +375,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
                   );
                 },
                 reason => {
-                  sendError(event.id, { code: 4001, message: reason });
+                  sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: reason });
                 },
               );
             } else {
@@ -383,7 +390,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
                   );
                 },
                 reason => {
-                  sendError(event.id, { code: 4001, message: reason });
+                  sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: reason });
                 },
               );
             }
@@ -395,11 +402,11 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             _ => {
 
-              sendError(event.id, { code: 4001, message: 'Not implemented' });
+              sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: 'Not implemented' });
             },
             reason => {
 
-              sendError(event.id, { code: 4001, message: reason });
+              sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: reason });
             },
           );
           break;
@@ -410,7 +417,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
               handleSignMessage.current(event.id, event.object.data, info.decryptedPhrase);
             },
             reason => {
-              sendError(event.id, { code: 4001, message: reason });
+              sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: reason });
             },
           );
           break;
@@ -421,7 +428,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
               handleSignTypedMessage.current(event, info.decryptedPhrase);
             },
             reason => {
-              sendError(event.id, { code: 4001, message: reason });
+              sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: reason });
             },
           );
           break;
@@ -441,7 +448,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
                 );
             },
             reason => {
-              sendError(event.id, { code: 4001, message: reason });
+              sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: reason });
             },
           );
           break;
@@ -450,7 +457,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
             event,
             () => {},
             reason => {
-              sendError(event.id, { code: 4001, message: reason });
+              sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: reason });
             },
           );
           break;
@@ -514,7 +521,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
                 sendResponse(event.id);
               },
               reason => {
-                sendError(event.id, { code: 4001, message: reason });
+                sendError(event.id, { code: RPCErrorCode.USER_REJECTED_REQUEST, message: reason });
               },
             );
           }
@@ -524,7 +531,7 @@ export const useIPCProvider = (props: IUseIPCProviderProps) => {
             const foundConfig = chainConfigs.find(c => isHexEqual(c.chainId, event.object.chainId));
 
             if (!foundConfig) {
-              sendError(event.id, { code: 4902, message: 'Chain not found' });
+              sendError(event.id, { code: RPCErrorCode.REQUESTED_CHAIN_NOT_ADDED, message: 'Chain not found' });
               return;
             }
 
