4f0152ad3b7f2a44a1a420c2812107cce8dbacb0
Matthew To <matthew.to@crypto.com>
2024-03-21 10:58:59 +0800

    fix: audit

--- electron/utils.ts
+++ electron/utils.ts
@@ -1,4 +1,4 @@
-import { URL } from "url";
+import { URL } from 'url';
 
 interface ValidURLCheckResult {
   isValid: boolean;
@@ -8,16 +8,15 @@ interface ValidURLCheckResult {
 export function isValidURL(str: string): ValidURLCheckResult {
   try {
     if (!str.startsWith('https://') && !str.startsWith('http://')) {
-      str = 'https://' + str
-
+      str = 'https://' + str;
     }
-    const parsedUrl = new URL(str)
-    const regex = /^([a-zA-Z0-9-_.:]+)+$/
+    const parsedUrl = new URL(str);
+    const regex = /^([a-zA-Z0-9-_.:]+)+$/;
     return {
       isValid: regex.test(parsedUrl.host),
       finalURL: str
-    }
+    };
   } catch (e) {
-    return { isValid: false, finalURL: str }
+    return { isValid: false, finalURL: str };
   }
 }
