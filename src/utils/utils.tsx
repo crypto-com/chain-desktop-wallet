15fd26d4da73306302155ef44f6ea1ab744e5bc3
Matthew To <matthew.to@crypto.com>
2024-03-25 17:49:33 +0800

    fix: audit

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -314,17 +314,16 @@ interface ValidURLCheckResult {
 export function isValidURL(str: string): ValidURLCheckResult {
   try {
     if (!str.startsWith('https://') && !str.startsWith('http://')) {
-      str = 'https://' + str
-
+      str = 'https://' + str;
     }
-    const parsedUrl = new URL(str)
-    const regex = /^([a-zA-Z0-9-_.:]+)+$/
+    const parsedUrl = new URL(str);
+    const regex = /^([a-zA-Z0-9-_.:]+)+$/;
     return {
       isValid: regex.test(parsedUrl.host),
       finalURL: str
-    }
+    };
   } catch (e) {
-    return { isValid: false, finalURL: str }
+    return { isValid: false, finalURL: str };
   }
 }
 
